7d8c50eb91b1f87ea1a6f7c9ddc091891d8a6b20
Momen <touristmomen@gmail.com>
2025-06-14 19:39:53 +0600

    FeaturedThisWeek updated

--- src/app/(main)/documentaries/FeaturedThisWeek.jsx
+++ src/app/(main)/documentaries/FeaturedThisWeek.jsx
@@ -1,31 +1,27 @@
-const featuredNews = [
-  {
-    id: 1,
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_4FBis8oXDCG39aw3z-PmKNBDv65t4-uCDg&s",
-    title: "UK Treasury's Cryptocurrency Holdings",
-  },
-  {
-    id: 2,
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_4FBis8oXDCG39aw3z-PmKNBDv65t4-uCDg&s",
-    title: "UK Treasury's Cryptocurrency Holdings",
-  },
-  {
-    id: 3,
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_4FBis8oXDCG39aw3z-PmKNBDv65t4-uCDg&s",
-    title: "UK Treasury's Cryptocurrency Holdings",
-  },
-  {
-    id: 4,
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_4FBis8oXDCG39aw3z-PmKNBDv65t4-uCDg&s",
-    title: "UK Treasury's Cryptocurrency Holdings",
-  },
-];
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchFeaturedNews = async () => {
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/featured-this-week`
+  );
+  return data;
+};
 
 const FeaturedThisWeek = () => {
+  const {
+    data: featuredNews = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["featuredNews"],
+    queryFn: fetchFeaturedNews,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Failed to load.</div>;
+
   return (
     <div>
       <h2 className="text-xl sm:text-2xl font-bold text-[#5E17EB] mb-3 sm:mb-4 mt-12">
@@ -33,13 +29,16 @@ const FeaturedThisWeek = () => {
       </h2>
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
         {featuredNews.map((news) => (
-          <div
+          <a
             key={news.id}
+            href={news.link}
             className="flex items-center space-x-2 sm:space-x-3 group hover:scale-[1.02] sm:hover:scale-105 transition-transform duration-300"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <div className="w-20 sm:w-28 h-16 sm:h-20 flex-shrink-0 rounded-lg sm:rounded-xl overflow-hidden">
               <img
-                src={news.image}
+                src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/Images/${news.image}`}
                 alt={news.title}
                 className="w-full h-full object-cover"
               />
@@ -47,7 +46,7 @@ const FeaturedThisWeek = () => {
             <h3 className="text-[#5E17EB] text-xs sm:text-sm font-medium flex-1 line-clamp-2">
               {news.title}
             </h3>
-          </div>
+          </a>
         ))}
       </div>
     </div>
